759df7297bcfed89f13409a7d963e6cfe3cd7b92
chaitra <chaitrac1095@gmail.com>
2017-03-20 10:59:54 +0530

    Second commit

--- Inventory/repository/InventoryRepository.java
+++ Inventory/repository/InventoryRepository.java
@@ -1,16 +1,9 @@
 package com.trial.repository;
-
-/**
- * Created by technology on 7/3/17.
- */
-
 import com.trial.model.Inventory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface InventoryRepositoryImpl extends MongoRepository<Inventory,String>
 {
-   // public Inventory findById(String id);
 }
